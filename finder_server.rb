4174d140e0d3947d6c393b51ccfffec4ab56540a
mau5atron <gabrielbetancourt96@outlook.com>
2017-10-11 23:09:57 -0400

    First commit

---
+++ finder_server.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'nokogiri'
+require 'httparty'
+
+get '/'do
+p params[:job]
+# posts security jobs
+url = "https://miami.craigslist.org/search/#{params[:job]}"
+response = HTTParty.get url
+
+dom = Nokogiri::HTML(response.body)
+i = 0
+
+info = dom.css('.content').css('.result-row').css('.hdrlnk').map do |a|
+  "<p> #{i += 1}. #{a.text} #{a['href']}</p>"
+  
+   # increments job number list, #href lists url as well
+
+
+end
+
+
+
+
+end
